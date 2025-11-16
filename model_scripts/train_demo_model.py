69f534bbde1908175aec03f9f964dc46854ba952
Troy <troymuehlbauer1@gmail.com>
2025-11-15 21:50:41 -0800

    fix file path
    bruh moment

--- model_scripts/train_demo_model.py
+++ model_scripts/train_demo_model.py
@@ -3,7 +3,7 @@ from xgboost import XGBRegressor
 import pandas as pd
 
 # Load and train your model (example)
-df = pd.read_csv("data/processed/games_master.csv")
+df = pd.read_csv("../data/processed/games_master.csv")
 features = ["elo_diff", "is_neutral"]
 target = "score_diff"
 
@@ -14,5 +14,5 @@ model = XGBRegressor(n_estimators=300, max_depth=6, learning_rate=0.05)
 model.fit(X, y)
 
 # Save to disk
-joblib.dump(model, "data/models/score_diff_model.joblib")
+joblib.dump(model, "../data/models/score_diff_model.joblib")
 print("Model saved!")
